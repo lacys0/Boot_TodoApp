6f3470412a10575f45f7da03e4ad55fec3ba218e
Laszlo Talpa <talpalaszlo08@gmail.com>
2025-06-26 13:59:32 +0200

    Implemented base entities.

---
+++ src/main/java/com/laszlo/todoapp/model/Subject.java
@@ -0,0 +1,45 @@
+package com.laszlo.todoapp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // Relationship: A Subject can have many TodoItems
+    // mappedBy indicates the field in TodoItem that owns the relationship
+    // cascade = CascadeType.ALL means operations (like delete) on Subject will cascade to associated TodoItems
+    // orphanRemoval = true means if a TodoItem is disassociated from a Subject, it will be removed from the DB
+    @OneToMany(mappedBy = "subject", cascade = jakarta.persistence.CascadeType.ALL, orphanRemoval = true)
+    private Set<TodoItem> todoItems = new HashSet<>(); // Initialize to prevent NullPointerException
+
+    // You might want a constructor that just takes the name
+    public Subject(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name; // Useful for Thymeleaf select box display
+    }
+}
---
+++ src/main/java/com/laszlo/todoapp/model/TodoItem.java
@@ -0,0 +1,34 @@
+package com.laszlo.todoapp.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id; // New import
+import jakarta.persistence.JoinColumn;   // New import
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private LocalDate dueDate;
+
+    // New: Many TodoItems can belong to one Subject
+    @ManyToOne
+    @JoinColumn(name = "subject_id") // This specifies the foreign key column name in the todo_item table
+    private Subject subject; // This will hold the associated Subject object
+}
